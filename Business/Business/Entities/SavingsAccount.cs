4d782b11a364dfb817d9355d51a63bd534669b52
stRafa <111309339+stRafa@users.noreply.github.com>
2022-09-13 16:53:15 -0300

    Add files via upload

---
+++ Business/Business/Entities/SavingsAccount.cs
@@ -0,0 +1,17 @@
+﻿namespace Business.Entities
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+        
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
